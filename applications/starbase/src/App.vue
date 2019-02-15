8cec07e73eb11fe649b878b9dd4653ca903a2b82
xavii4all <jpelliza.gcba@gmail.com>
2019-02-15 20:45:07 -0300

    avances sobre starbase

---
+++ applications/starbase/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <h3> {{title}} </h3>
+        <Character />
+    </div>    
+</template>
+
+<script>
+import Character from './components/Character.vue'
+
+export default {
+    name: 'app',
+    data(){
+        return {
+            title: 'Generate Your Team'
+        }
+    },
+    components: {
+        Character
+    }
+}
+</script>
---
+++ applications/starbase/src/components/Character.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        {{character}}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            character: {}
+        }
+    },
+    methods: {
+        fetchCharacter() {
+            let proxy = 'https://cors-anywhere.herokuapp.com/';
+            fetch(proxy+'http://swapi.co/api/people', {
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(json => this.character = json)
+        }
+    },
+    created(){
+        this.fetchCharacter()
+    }
+}
+</script>
