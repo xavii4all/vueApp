d43c53a1211fd95b7416c1db7c65cefafd3f624f
jpelliza <jpelliza.gcba@gmail.com>
2019-02-20 16:43:19 -0300

    Starbase terminada

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,19 +1,24 @@
 <template>
     <div id="app">
         <h3> {{title}} </h3>
-        <Character :id="1"/>
-    </div>    
+        <div class="col-md-12">
+            <Character 
+                v-for="(id, index) in initial_ids" :id="id" :key="index"
+            />
+        </div>
+    </div>
 </template>
 
 
 <script>
-import Character from './components/Character.vue'
+import Character from './components/Character.vue';
 
 export default {
     name: 'app',
     data(){
         return {
-            title: 'Generate Your Team'
+            title: 'Generate Your Team',
+            initial_ids: [1, 13, 14]
         }
     },
     components: {
--- applications/starbase/src/components/Character.vue
+++ applications/starbase/src/components/Character.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-        {{character}}
+    <div class="col-md-4" @click="switchCharacter">
+        <!-- {{character}} -->
+        <div class="character-card">
+            <div class="card-block">
+                <h4 class="card-title"> {{character.name}} </h4>
+                <p class="card-text">Height: {{character.height}}cm </p>
+                <p class="card-text">Mass: {{character.mass}}kg </p>
+                <p class="card-text">Hair Color: {{character.hair_color}} </p>
+                <p class="card-text">Eye Color: {{character.eye_color}} </p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -20,6 +29,10 @@ export default {
             })
             .then(response => response.json())
             .then(json => this.character = json)
+        },
+        switchCharacter() {
+            let random_id = Math.floor(Math.random() * 83) +1;
+            this.fetchCharacter(random_id);
         }
     },
     created(){
